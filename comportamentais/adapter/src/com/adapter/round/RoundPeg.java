d29c386208eec2b5cabf111810b96b06e8a72891
Rafael Caetano <engcaetano@outlook.com>
2021-03-18 22:37:58 -0300

    Pattern Adapter

---
+++ comportamentais/adapter/src/com/adapter/round/RoundPeg.java
@@ -0,0 +1,17 @@
+package com.adapter.round;
+
+public class RoundPeg {
+
+    private double radius;
+
+    public RoundPeg() {
+    }
+
+    public RoundPeg(float radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
